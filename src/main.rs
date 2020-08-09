80ad218bc82d6b6cba879de549216b7edd32721b
Grecco <lucasgrecco@gmail.com>
2020-08-09 16:25:57 -0300

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+    let error_message = "Failed to read line.";
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect(&error_message);
+
+    println!("your guess: {}", guess);
+}
