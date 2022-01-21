309d8458b1d68a720bedce1c9709b978359b2356
Lucas Grecco <lucasgrecco@gmail.com>
2022-01-21 16:42:13 -0300

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -12,4 +12,38 @@ fn main() {
     }
     println!("The value of x {}", x);
 
+    /* *
+        Types - Integer
+    * */
+    let int_var: u32 = 12555;
+    println!("{}", int_var);
+    let int_var: i32 = -12555 + 555;
+    println!("{}", int_var);
+
+    /* *
+        Types - Float
+    * */
+    let float_var: f64 = 3.666457547547547547547547547;
+    println!("{}", float_var);
+
+    println!("{}", float_var - 2.64555);
+
+    /* *
+        Types - Tuples
+    * */
+    let tuple = (1, 2, 3, 4, 5, "Text");
+    println!("Tuple position 0: {}", tuple.0);
+    println!("Tuple position 1: {}", tuple.1);
+    println!("Tuple position 2: {}", tuple.2);
+    println!("Tuple position 3: {}", tuple.3);
+    println!("Tuple position 4: {}", tuple.4);
+    println!("Tuple position 5: {}", tuple.5);
+
+    /* *
+        Types - Arrays (must be the same type)
+    * */
+    let array_i32: [i32; 5] = [0, 1, 2, 3, 4];
+    println!("Array position 0: {}", array_i32[0]);
+
+
 }
